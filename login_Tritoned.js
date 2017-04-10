65c7dcfa01d5302567e6e8ad798cafacd1c1cdf8
hy299792458 <hy299792458@gmail.com>
2017-04-09 17:13:11 -0700

    small changes

--- login_Tritoned.js
+++ login_Tritoned.js
@@ -1,17 +1,10 @@
 // Auto login for tritoned
 
 (function (chrome) {
-
     loginField = document.getElementById('user_id');
     passField = document.getElementById('password');
     var btm = document.getElementById('entry-login');
-    chrome.extension.sendRequest({method: "username"}, function(response) {
-      var username = user;
-      loginField.value = username;
-    });
-    chrome.extension.sendRequest({method: "password"}, function(response) {
-      var password = password;
-      passField.value = password;
-      btm.click();
-    });
+    loginField.value = "yah072";
+    passField.value = "10136@CJ";
+    btm.click();
 }(chrome));
--- login_Tritonlink.js
+++ login_Tritonlink.js
@@ -1,19 +1,12 @@
 // Auto login for mytritonlink
 
 (function (chrome) {
-    var test = 0;
     loginField = document.getElementById('ssousername');
     passField = document.getElementById('ssopassword');
     var btm = document.getElementsByTagName('input')[3];
-    chrome.extension.sendRequest({method: "username"}, function(response) {
-      var username = username;
-      loginField.value = username;
-    });
-    chrome.extension.sendRequest({method: "password"}, function(response) {
-      var password = password;
-      passField.value = password;
-      if (test != 0){
-          btm.click();
-      };
-    });
+    var username = username;
+    loginField.value = "yah072";
+    var password = password;
+    passField.value = "10136@CJ";
+    btm.click();
 }(chrome));
