43dc4e4163c0497a849fb873f1887ad2e27fa342
hy299792458 <hy299792458@gmail.com>
2017-04-12 23:21:36 -0700

    add notice

--- background.js
+++ background.js
@@ -10,6 +10,10 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             password: localStorage["password"]
         })};
         senttime[request.user[0]] = request.user[1];
+    };
+    if (request.badges){
+        updatebadge();
+        sendResponse({badges: localStorage["badges"]});
     }
 });
 
@@ -17,6 +21,28 @@ document.querySelector("#Login").addEventListener('click', function (){
     localStorage.setItem("username", document.getElementById("username").value);
     localStorage.setItem("password", document.getElementById("password").value);
 });
+
+//remember the last input
 document.getElementById("username").value=localStorage["username"];
 document.getElementById("password").value=localStorage["password"];
-
+function updatebadge() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var badgexmlhttp = new XMLHttpRequest();
+            badgexmlhttp.onreadystatechange = function () {
+                if (badgexmlhttp.readyState == 4 && badgexmlhttp.status == 200) {
+                    /:(\w+),/gm.exec(badgexmlhttp.responseText);
+                    //alert(RegExp.$1);
+                    localStorage.setItem("badges", RegExp.$1);
+                }
+            }
+            badgexmlhttp.open("POST", "https://tritoned.ucsd.edu/webapps/portal/dwr_open/call/plaincall/ToolActivityService.getActivityForAllTools.dwr", true);
+            badgexmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            badgexmlhttp.send('callCount=1&page=/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_1_1&httpSessionId=73B736AA5FC4D6119E9CC362CC38CC3B&scriptSessionId=8A22AEE4C7B3F9CA3A094735175A6B14249&c0-scriptName=ToolActivityService&c0-methodName=getActivityForAllTools&c0-id=0&batchId=1');
+        }
+    }
+    xmlhttp.open("POST", "https://tritoned.ucsd.edu/webapps/login/", true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send("action=login&login=Login&new_loc=&password=10136%40CJ&user_id=yah072");
+}
